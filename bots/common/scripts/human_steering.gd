776b71e2439cba8fd271798d31101e3c3829e861
Leroy Hopson <git@leroy.geek.nz>
2020-08-22 10:25:55 +0700

    wip

--- bots/common/scripts/human_steering.gd
+++ bots/common/scripts/human_steering.gd
@@ -6,8 +6,6 @@ export(int) var max_speed := 50
 export(int) var acceleration_factor := 15
 export(bool) var ai_controlled := false
 
-var enemy: Spatial
-var prev_d: float
 
 onready var _primary_weapon = get_node_or_null(primary_weapon)
 onready var _left_wheels := [$WheelFrontLeft, $WheelRearLeft]
@@ -27,43 +25,12 @@ func _process(delta):
 
 
 func _physics_process(delta):
-	if ai_controlled:
-		_ai_physics_process(delta)
-	else:
-		_human_physics_process(delta)
-
-
-func _ai_physics_process(delta):
-	# Turreting process.
-	var fA = global_transform.basis.z
-	var AE = global_transform.origin.direction_to(enemy.global_transform.origin)
-	var d = AE.dot(fA)
-	# When d == 1 we are facing the enemy. So toggle sticks to make this 1.
-	print("d: ", d, " AE.z: ", AE.z)
-	if d < 0.99:
-		# Not facing
-		var rate = 1 - log(abs(d))
-		if AE.z > 0:
-			# Turn right
-			_accelerate(delta, -1 * rate, 1 * rate)
-		else:
-			# Turn left
-			_accelerate(delta, 1 * rate, -1 * rate)
-		
-		prev_d = d
-
-
-func _human_physics_process(delta):
 	# Handle steering. For now just use tank steering.
 	# Left stick controls left wheels.
 	# Right stick controls right wheels.
 	var left_acceleration = Input.get_joy_axis(0, JOY_AXIS_1)
 	var right_acceleration = Input.get_joy_axis(0, JOY_AXIS_4)
 	
-	_accelerate(delta, left_acceleration, right_acceleration)
-
-
-func _accelerate(delta, left_acceleration, right_acceleration):
 	for wheel in _left_wheels:
 		if left_acceleration == 0 and wheel.engine_force > 0:
 			wheel.engine_force = 0
--- bots/hammer_bot.gd
+++ bots/hammer_bot.gd
@@ -1,6 +1,9 @@
 extends Spatial
 
 
+export(bool) var ai_controlled := false
+export(NodePath) var enemy: NodePath
+
 const RESET_FORCE := -1.5
 const FIRE_FORCE := 1000
 
@@ -8,7 +11,19 @@ const FIRE_FORCE := 1000
 var _hammer_fired := false
 
 
+func _ready():
+	if ai_controlled:
+		$Chassis.ai_controlled = true
+		var e = get_node(enemy)
+		$Chassis.enemy = e.get_node("Chassis")
+	else:
+		$Chassis.ai_controlled = false
+
+
 func _physics_process(delta):
+	if ai_controlled:
+		return
+	
 	if Input.is_action_just_pressed("bot_weapon_primary"):
 		if _hammer_fired:
 			$HingeJoint.set_param(HingeJoint.PARAM_MOTOR_TARGET_VELOCITY, RESET_FORCE)
